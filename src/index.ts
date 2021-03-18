78a88e0579624099fbff8763a534ec8864ff60f6
Marvin T <contact@marvinkturner.com>
2021-03-18 00:01:18 -0400

    fix: move demo code to right file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 class Sorter {
-  constructor(collection: number[]) {}
+  constructor(public collection: number[]) {}
 
   sort(): void {
     
   }
 }
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
