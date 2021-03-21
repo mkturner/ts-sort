d717f6c8c9a9a2f2b90e0280e7fdbc6c90a49bc9
Marvin T <contact@marvinkturner.com>
2021-03-20 23:59:09 -0400

    feat: add support for strings

--- build/index.js
+++ build/index.js
@@ -1,11 +1,8 @@
-"use strict";
 var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-    }
-    Sorter.prototype.sort = function () {
-    };
-    return Sorter;
-}());
+  function Sorter(collection) {}
+  Sorter.prototype.sort = function () {};
+  return Sorter;
+})();
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     //  grab length of collection
@@ -7,10 +7,18 @@ class Sorter {
 
     for (let i=0; i<length; i++) {
       for (let j=0; j<length-i-1; j++) {
-        if (this.collection[j] > this.collection[j+1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = temp;
+
+        // If collection is an array of numbers
+        if (this.collection instanceof Array) { // type guard
+          if (this.collection[j] > this.collection[j+1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j+1];
+            this.collection[j+1] = temp;
+          }
+        }
+        // If collection is a string
+        if (typeof this.collection == 'string') {
+            //
         }
       }
     }
